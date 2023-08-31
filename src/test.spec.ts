5944d119ba07232764b77ce4914a8190f11e542b
novarx <info@novarx.ch>
2023-08-31 02:55:55 +0200

    update jest config to support typescript

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,5 +1,15 @@
-describe('MyClass', () => {
-  it('does', () => {
-    expect(true).toBeTruthy();
-  });
+class ValueObject<T> {
+    constructor(private _value: T) {
+    }
+
+    get value(): T {
+        return this._value;
+    }
+}
+
+describe('ValueObject', () => {
+    it('should wrap a string value', () => {
+        const sut = new ValueObject<string>('lorem ipsum');
+        expect(sut.value).toEqual('lorem ipsum');
+    });
 });
